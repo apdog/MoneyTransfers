d48072d5aa67575e7fcec89b24e28b9a0d7cbeec
“Apdog” <apdog@ya.ru>
2024-03-17 20:56:36 +0300

    Commission calculation added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 package ru.netology
 
 fun main() {
-    println("Hello World!")
+    val amount = 10000.0
+    commissionCalculation(amount)
+}
+
+fun commissionCalculation(amount: Double) {
+    val commissionRate = 0.0075
+    val minCommission = 35.0
+    val result = amount * commissionRate
+    val commission = if (result < minCommission) minCommission else result
+    println("Комиссия с перевода $amount рублей составила $commission рублей")
 }
